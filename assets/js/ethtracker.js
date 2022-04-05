1f0445a8d8e93c612a4901253461673b4a4958c8
avpixel <avpixel@outlook.com>
2022-04-05 15:22:26 +0700

    Update ethtracker.js
    Update ETH Wallet address

--- assets/js/ethtracker.js
+++ assets/js/ethtracker.js
@@ -1,7 +1,7 @@
 function checkEthBalance() {
     document.getElementById("ethBalance").innerHTML = "Xin vui lòng chờ.....";
 
-    fetch( 'https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=XDQVUR479WPCQXZNP4AMVWJ143YM8KMJM2' )
+    fetch( 'https://api.etherscan.io/api?module=account&action=balance&address=0xc161896f06dec5fe38ef2c4aca4100fee84944ec&tag=latest&apikey=XDQVUR479WPCQXZNP4AMVWJ143YM8KMJM2' )
         .then( response => response.json() )
         .then( response => {
             document.getElementById("ethBalance").innerHTML = "Số lượng ETH trong ví: " + response.result;
